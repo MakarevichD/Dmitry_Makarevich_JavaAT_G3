0288e8b76f74844e3027dae278a1ebfba51ed64f
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-06-19 02:22:39 +0300

    nice pony

---
+++ main/java/tasks/homework_week2/HorsePicture.java
@@ -0,0 +1,15 @@
+package main.java.tasks.homework_week2;
+
+public class HorsePicture {
+
+    public void PonyHorse() {
+
+        System.out.println(" " + "^__^");
+        System.out.println(" (oo)\\\\_______");
+        System.out.println("(__)\\) \\/\\ \\");
+        System.out.println("    ||----w|\\ ");
+        System.out.println("    ||     ||");
+
+    }
+}
+
