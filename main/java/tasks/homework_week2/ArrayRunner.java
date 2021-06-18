68e9a776ff8e7e8d4bed496be7f8deb221fa4412
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-06-19 02:23:37 +0300

    new runner for arrays

---
+++ main/java/tasks/homework_week2/ArrayRunner.java
@@ -0,0 +1,11 @@
+package main.java.tasks.homework_week2;
+
+public class ArrayRunner {
+
+    public static void main(String[] args) {
+
+        new nElementsArraySum().nRandomArray(3);
+        new SumByMM().CurrentMonthDate(6);
+        new HorsePicture().PonyHorse();
+    }
+}
