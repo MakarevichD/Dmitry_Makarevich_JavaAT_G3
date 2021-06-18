6ace8aba148f47f36a11b68c730b654df1f067d0
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-06-19 02:41:09 +0300

    new factory building

--- main/java/projects/factory/BottlingWater.java
+++ main/java/projects/factory/BottlingWater.java
@@ -1,2 +1,18 @@
-package main.java.projects.factory;public class BottlingWater {
+package main.java.projects.factory;
+
+import main.java.projects.boxing.Bottle;
+
+public class BottlingWater {
+
+    public static void main(String[] args) {
+
+        Bottle bottleS = new Bottle(0.5);
+        Bottle bottleM = new Bottle(1.0);
+        Bottle bottleL = new Bottle(1.5);
+
+        bottleS.warm(15);
+        bottleM.warm(4);
+        bottleL.warm(22);
+
+    }
 }
