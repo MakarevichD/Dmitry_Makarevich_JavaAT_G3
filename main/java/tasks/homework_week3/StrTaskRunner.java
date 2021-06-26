43718d9059d8aea2bd97a191bb0208011dd19dca
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-06-26 12:19:20 +0300

    new method added to runner

--- main/java/tasks/homework_week3/StrTaskRunner.java
+++ main/java/tasks/homework_week3/StrTaskRunner.java
@@ -1,12 +1,17 @@
 package main.java.tasks.homework_week3;
 
+import java.text.ParseException;
+
 public class StrTaskRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException{
 
         new ArrayReplacement().NumWithLetters();
         System.out.println();
         new DuplicateWords().StringSentense();
+        System.out.println();
+        new CurrentDateNow().ShowTimeDate();
+
 
     }
 }
