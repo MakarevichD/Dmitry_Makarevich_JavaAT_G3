6fbe575adb6264a7875514df6011c0a5a245f553
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-06-26 12:18:01 +0300

    new date added

--- main/java/tasks/homework_week3/CurrentDateNow.java
+++ main/java/tasks/homework_week3/CurrentDateNow.java
@@ -1,19 +1,25 @@
 package main.java.tasks.homework_week3;
 
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
 public class CurrentDateNow {
 
-    public void ShowTimeDate (){
+    public void ShowTimeDate(){
 
+        String pattern = "dd MMMMM yyyy HH:mm:ss";
+        SimpleDateFormat simpleDateFormat =
+                new SimpleDateFormat(pattern, new Locale("en", "BY"));
 
+        String date = simpleDateFormat.format(new Date());
+        System.out.println("Сейчас на дворе:" + date );
+       // System.out.println(date);
     }
 
 
-
-
-
-
-
-
-    }
+}
 
