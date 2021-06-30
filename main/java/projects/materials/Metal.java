9ad20b4798a5f39d9ba81c66ba18a147628a17ec
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-06-30 18:06:16 +0300

    get added?

--- main/java/projects/materials/Metal.java
+++ main/java/projects/materials/Metal.java
@@ -9,5 +9,7 @@ public class Metal extends Material {
 
     public Metal(double thermalConductivity, String color, double density) {
         super(thermalConductivity, color, density);
+        this.getThermalConductivity();
+
     }
 }
--- main/java/projects/materials/Plastic.java
+++ main/java/projects/materials/Plastic.java
@@ -9,5 +9,7 @@ public class Plastic extends Material {
 
     public Plastic(double thermalConductivity, String color, double density) {
         super(thermalConductivity, color, density);
+
+
     }
 }
