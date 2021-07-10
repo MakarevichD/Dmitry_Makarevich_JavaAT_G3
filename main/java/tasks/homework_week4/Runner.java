b625382c57baf8b2ac090bb139ba6c4188aa4327
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-07-10 14:25:11 +0300

    week4 added

--- main/java/tasks/homework_week4/Runner.java
+++ main/java/tasks/homework_week4/Runner.java
@@ -1,2 +1,12 @@
-package main.java.tasks.homework_week4;public class Runner {
+package main.java.tasks.homework_week4;
+
+import java.io.IOException;
+
+public class Runner {
+
+    public static void main(String[] args) throws IOException {
+
+        new ScanNextMonth().printNextMonth();
+        new FoldersData().DeepFolderFiles();
+    }
 }
