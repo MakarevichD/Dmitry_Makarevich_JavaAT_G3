ca690f44b2d1b88f5191ffb742385636927787d4
DmitryMakarevich <dmitro.makarevich@gmail.com>
2021-07-15 00:22:01 +0300

    inches task

--- main/java/tasks/homework_week6/InchToCm.java
+++ main/java/tasks/homework_week6/InchToCm.java
@@ -1,2 +1,23 @@
-package main.java.tasks.homework_week6;public class InchToCm {
+package main.java.tasks.homework_week6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class InchToCm {
+
+    public static void main(String[] args) {
+
+        int n = 1;
+        int c = 21;
+        IntStream.range(n, c)
+                .filter(i -> i % 2 == 0)
+                .peek(i -> System.out.println(i * 2.54 + " cm in inch"))
+                .reduce((s1, s2) -> s1 + s2)
+                .ifPresent(System.out::println);
+
+
+    }
 }
